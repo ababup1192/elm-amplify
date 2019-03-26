87b136def3392fed9c029528e1b79370e9647076
abab <ababup1192@gmail.com>
2019-03-26 09:20:29 +0900

    remove code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 'use strict';
 import { environment } from './environments'
 import Amplify, { Auth } from 'aws-amplify';
-import { Observable, BehaviorSubject, from, of, map, catchError, mergeMap } from 'rxjs';
+import { Observable, from, of } from 'rxjs';
 import * as axios from 'axios';
 
 require("./styles.scss");
@@ -11,10 +11,7 @@ var app = Elm.Main.init();
 
 Amplify.configure(environment.amplify);
 
-const loggedIn = new BehaviorSubject(false);
-const signIn$ = from(Auth.signIn('user', 'pass'));
-
-signIn$.subscribe(signIn => {
+from(Auth.signIn('user', 'pass')).subscribe(signIn => {
   const instance = axios.create({
     baseURL: environment.apiBaseUrl,
     headers: {
