0ee59fddc6209eba2f17188470097bb793085f8d
Mirai Watanabe <ababup1192@gmail.com>
2019-03-26 14:36:47 +0900

    Finish amplify

--- src/Main.elm
+++ src/Main.elm
@@ -5,6 +5,8 @@ import Browser.Navigation as Nav
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
+import Http
+import Json.Decode as JD
 
 
 
@@ -13,13 +15,66 @@ import Html.Events exposing (..)
 -- ---------------------------
 
 
+type alias User =
+    { username : String
+    , email : String
+    , password : String
+    }
+
+
+type alias SignInUser =
+    { username : String
+    , password : String
+    }
+
+
+type alias ConfirmInfo =
+    { username : String
+    , code : String
+    }
+
+
 type alias Model =
-    Int
+    { username : String
+    , code : String
+    , email : String
+    , password : String
+    , token : String
+    , apiBaseUrl : String
+    , pets : List Pet
+    }
 
 
-init : () -> ( Model, Cmd Msg )
-init _ =
-    ( 0, Cmd.none )
+type alias Flags =
+    { apiBaseUrl : String
+    }
+
+
+type alias Pet =
+    { id : Int
+    , animalType : String
+    , price : Float
+    }
+
+
+init : Flags -> ( Model, Cmd Msg )
+init { apiBaseUrl } =
+    ( Model "" "" "" "" "" apiBaseUrl [], Cmd.none )
+
+
+port signup : User -> Cmd msg
+
+
+port signin : SignInUser -> Cmd msg
+
+
+port confirm : ConfirmInfo -> Cmd msg
+
+
+port sendToken : (String -> msg) -> Sub msg
+
+
+port signout : () -> Cmd msg
 
 
 
@@ -28,19 +83,84 @@ init _ =
 -- ---------------------------
 
 
+getPets : String -> String -> Cmd Msg
+getPets apiBaseUrl token =
+    Http.request
+        { method = "GET"
+        , headers =
+            [ Http.header "Authorization" token
+            ]
+        , url = apiBaseUrl ++ "/pets"
+        , body = Http.emptyBody
+        , expect = Http.expectJson GotPets decodePets
+        , timeout = Nothing
+        , tracker = Nothing
+        }
+
+
+decodePets : JD.Decoder (List Pet)
+decodePets =
+    JD.list decodePet
+
+
+decodePet : JD.Decoder Pet
+decodePet =
+    JD.map3 Pet
+        (JD.field "id" JD.int)
+        (JD.field "type" JD.string)
+        (JD.field "price" JD.float)
+
+
 type Msg
-    = Increment
-    | Decrement
+    = SignUp
+    | Confirm
+    | SignIn
+    | SignOut
+    | UpdateUserName String
+    | UpdateEmail String
+    | UpdatePassword String
+    | UpdateCode String
+    | GetToken String
+    | GotPets (Result Http.Error (List Pet))
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
+update msg ({ username, email, password, code, token, apiBaseUrl } as model) =
     case msg of
-        Increment ->
-            ( model + 1, Cmd.none )
+        SignUp ->
+            ( model, signup { username = username, email = email, password = password } )
 
-        Decrement ->
-            ( model - 1, Cmd.none )
+        Confirm ->
+            ( model, confirm { username = username, code = code } )
+
+        SignIn ->
+            ( model, signin { username = username, password = password } )
+
+        SignOut ->
+            ( model, signout () )
+
+        UpdateUserName uname ->
+            ( { model | username = uname }, Cmd.none )
+
+        UpdateEmail em ->
+            ( { model | email = em }, Cmd.none )
+
+        UpdatePassword pwd ->
+            ( { model | password = pwd }, Cmd.none )
+
+        UpdateCode c ->
+            ( { model | code = c }, Cmd.none )
+
+        GetToken t ->
+            ( { model | token = t }, getPets apiBaseUrl t )
+
+        GotPets result ->
+            case result of
+                Ok pets ->
+                    ( { model | pets = pets }, Cmd.none )
+
+                Err e ->
+                    ( model, Cmd.none )
 
 
 
@@ -50,11 +170,33 @@ update msg model =
 
 
 view : Model -> Html Msg
-view model =
+view { username, password, email, code, pets } =
     div [ class "container" ]
-        [ button [ onClick Increment ] [ text "+" ]
-        , p [] [ text <| String.fromInt model ]
-        , button [ onClick Decrement ] [ text "-" ]
+        [ div []
+            [ input [ placeholder "username", onInput UpdateUserName, value username ] []
+            , input [ placeholder "email", type_ "email", onInput UpdateEmail, value email ] []
+            , input [ placeholder "password", type_ "password", onInput UpdatePassword, value password ] []
+            , input [ type_ "button", value "sign up !!", onClick SignUp ] []
+            ]
+        , div []
+            [ input [ placeholder "username", onInput UpdateUserName, value username ] []
+            , input [ placeholder "code", type_ "number", onInput UpdateCode, value code ] []
+            , input [ type_ "button", value "confirm !!", onClick Confirm ] []
+            ]
+        , div []
+            [ input [ placeholder "username", onInput UpdateUserName, value username ] []
+            , input [ placeholder "password", type_ "password", onInput UpdatePassword, value password ] []
+            , input [ type_ "button", value "sign in !!", onClick SignIn ] []
+            ]
+        , div []
+            [ input [ type_ "button", value "sign out !!!", onClick SignOut ] []
+            ]
+        , ul [] <|
+            List.map
+                (\{ id, animalType, price } ->
+                    li [] [ text <| String.fromInt id ++ ", " ++ animalType ++ ", " ++ String.fromFloat price ]
+                )
+                pets
         ]
 
 
@@ -64,15 +206,20 @@ view model =
 -- ---------------------------
 
 
-main : Program () Model Msg
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    sendToken GetToken
+
+
+main : Program Flags Model Msg
 main =
     Browser.document
         { init = init
         , update = update
         , view =
             \m ->
-                { title = "カウンター"
+                { title = "Get Pets"
                 , body = [ view m ]
                 }
-        , subscriptions = \_ -> Sub.none
+        , subscriptions = subscriptions
         }
--- src/index.js
+++ src/index.js
@@ -7,20 +7,45 @@ import * as axios from 'axios';
 require("./styles.scss");
 
 const {Elm} = require('./Main');
-var app = Elm.Main.init();
+var app = Elm.Main.init({ flags: { apiBaseUrl: environment.apiBaseUrl } });
 
 Amplify.configure(environment.amplify);
 
-from(Auth.signIn('user', 'pass')).subscribe(signIn => {
-  const instance = axios.create({
-    baseURL: environment.apiBaseUrl,
-    headers: {
-      'Authorization': signIn.signInUserSession.idToken.jwtToken
-    }
+app.ports.signup.subscribe(user => {
+  const { username, email, password } = user;
+  console.log(username);
+  Auth.signUp({
+    username,
+    password,
+    attributes: {
+        email
+    },
+    validationData: []
+    })
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+});
+
+app.ports.confirm.subscribe(confirmInfo => {
+  const { username, code } =  confirmInfo;
+  Auth.confirmSignUp(username, code, {
+    forceAliasCreation: true
+}).then(data => console.log(data))
+  .catch(err => console.log(err));
+});
+
+app.ports.signin.subscribe(user => {
+  const { username, password } = user;
+  from(Auth.signIn(username, password)).subscribe(signIn => {
+    app.ports.sendToken.send(signIn.signInUserSession.idToken.jwtToken);
   });
-  instance.get('/pets').then(response =>
-    console.log(response)
-  ).catch(error =>
-    console.log(error)
-  );
+});
+
+
+app.ports.signout.subscribe(_ => {
+  Auth.signOut().then(() => {
+    this.setState({authState: 'signIn'});
+  }).catch(e => {
+    console.log(e);
+  });
 });
